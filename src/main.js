59099705de71cf6e5f723b417cb790ff5f22f565
Your Name <you@example.com>
2019-12-18 21:37:03 +0800

    bug 修复

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'// 引入模块
 import 'element-ui/lib/theme-chalk/index.css'// 引入样式
-import './styles/index' // 引入初始化样式
-Vue.config.productionTip = false
+// import './styles/index' // 引入初始化样式
+// import './styles/index.less'
+// import 'element-ui/lib/theme-chalk/index.less'
 Vue.use(ElementUI)
+Vue.config.productionTip = false
 
 new Vue({
   router,
