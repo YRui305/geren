d35cdc8d3eb716e27fde86cbee96b3116870bd8b
Your Name <you@example.com>
2019-12-21 11:18:21 +0800

    左侧导航栏菜单生成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,25 @@
 <template>
-    <div>主页</div>
+    <!-- <div>主页</div> -->
+    <!-- 放置一个大容器 -->
+    <el-container>
+        <el-aside style="height:100vh;background-color:#353b4e;width:230px">
+            <layout-aside></layout-aside>
+        </el-aside>
+        右侧容器
+        <el-container>
+            <!-- 上下布局 -->
+            <el-header>头部</el-header>
+            <el-min>主要区域</el-min>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
+import layoutAside from '../../components/layout-aside'
 export default {
-
+  components: {
+    'layout-aside': layoutAside
+  }
 }
 </script>
 
